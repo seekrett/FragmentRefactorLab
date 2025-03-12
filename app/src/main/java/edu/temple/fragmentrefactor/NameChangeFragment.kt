5500c5bda9a6da35d6695794c83368b5d9d48244
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-12 15:42:12 -0400

    moved functionality to fragment + connected to main

--- app/src/main/java/edu/temple/fragmentrefactor/NameChangeFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/NameChangeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class NameChangeFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,20 @@ class NameChangeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_name_change, container, false)
+        return inflater.inflate(R.layout.fragment_name_change, container, false).apply {
+            val displayTextView = findViewById<TextView>(R.id.displayTextView)
+            val nameEditText = findViewById<EditText>(R.id.nameEditText)
+            val changeButton = findViewById<Button>(R.id.changeButton)
+
+            changeButton.setOnClickListener {
+                val name = nameEditText.text
+
+                displayTextView.text = if (name.isNotBlank()) {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name"
+                }
+            }
+        }
     }
 }
